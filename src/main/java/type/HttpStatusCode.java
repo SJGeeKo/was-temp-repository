48d3ec7216f1e420b8922ba9524d39821f4c2963
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-02-08 19:31:18 +0900

    chore: HttpStatusCode 추가

--- src/main/java/type/HttpStatusCode.java
+++ src/main/java/type/HttpStatusCode.java
@@ -4,8 +4,9 @@ public enum HttpStatusCode {
     OK("OK", 200),
     CREATED("CREATED", 201),
     REDIRECT("REDIRECT", 302),
+    BAD_REQUEST("BAD REQUEST", 400),
     NOT_FOUND("NOT FOUND", 404),
-    ;
+    INTERNAL_SERVER_ERROR("INTERNAL SERVER ERROR", 500);
 
     private final String description;
     private final Integer code;
